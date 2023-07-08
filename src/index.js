dda7ec4633eb59b0a1ea228aab5937c77e29d6aa
Tanuj Ojha <tanujojha2042@gmail.com>
2023-07-08 13:53:39 +0530

    refactor strict mode index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <AuthContextProvider>
       <App />
-    </AuthContextProvider>,
-  // </React.StrictMode>,
+    </AuthContextProvider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
